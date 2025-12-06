a8588c7e0a51f610a9b0231b7b8cf2c8173e2397
baba-lena <arsen.nurmetov@gmail.com>
2025-12-06 20:26:25 +0300

    comment date test

--- src/test/java/OpenPageTests.java
+++ src/test/java/OpenPageTests.java
@@ -3,6 +3,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class OpenPageTests {
     @Test
     void openWebSite() {
-        open("https://ya.ru");
+        open("https://ya.ru"); //06.12.2025
     }
+
 }
