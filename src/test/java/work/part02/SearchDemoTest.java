52c1f9053e9d53f4ef34d1871c2d6cfff97a63f2
baba-lena <arsen.nurmetov@gmail.com>
2025-12-07 12:57:54 +0300

    work.part02: add testElementSearchMethods

---
+++ src/test/java/work/part02/SearchDemoTest.java
@@ -0,0 +1,26 @@
+package work.part02;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchDemoTest {
+    @Test
+    public void testElementSearchMethods() {
+        open("https://slqamsk.github.io/demo/search-demo/");
+        By myLocator = By.id("submit-button");
+        SelenideElement myElement = Selenide.element(myLocator);
+        myElement.shouldBe(visible);
+        element(By.id("submit-button")).shouldBe(visible);
+        $(By.id("submit-button")).shouldBe(visible);
+        $(By.name("interests")).shouldBe(visible);
+        $(By.className("btn")).shouldBe(visible);
+        $(By.tagName("h3")).shouldBe(visible);
+        $(By.linkText("Регистрация нового пользователя в системе")).shouldBe(visible);
+        $(By.partialLinkText("длинный")).shouldBe(visible);
+    }
+}
