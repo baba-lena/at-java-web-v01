e44f38004b222475568796e6f24d92f550f4f84b
baba-lena <arsen.nurmetov@gmail.com>
2025-12-21 14:12:44 +0300

    work.part07: add POM

--- src/test/java/work/part07/POMFlightsTests.java
+++ src/test/java/work/part07/POMFlightsTests.java
@@ -1,34 +1,22 @@
 package work.part07;
-
-
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import com.codeborne.selenide.logevents.SelenideLogger;
-
-import work.part07.LoginPage;
-import work.part07.SearchPage;
-
-
 import org.junit.jupiter.api.*;
-
 
 @TestMethodOrder(MethodOrderer.DisplayName.class)
 public class POMFlightsTests {
-
-
     @BeforeEach
-    void setUp() {
+    void setUp()
+    {
         open("https://slqamsk.github.io/cases/slflights/v01/");
         //getWebDriver().manage().window().maximize();
     }
 
     @Test
-    void test01() {
+    void test01()
+    {
         LoginPage loginPage = new LoginPage();
         loginPage.login("standard_user", "stand_pass1");
         loginPage.isLoginSuccessful("Иванов Иван Иванович");
-
     }
 
     @Test
@@ -41,7 +29,6 @@ public class POMFlightsTests {
         SearchPage searchPage = new SearchPage();
         searchPage.search("01.12.2025");
         searchPage.incorrectDate();
-
     }
 
 }
